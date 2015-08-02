1a5c12c37926ad6ea7b37788d059c663717f2ba0
Jhony Penagos <japenagoss@gmail.com>
2015-08-01 22:32:10 -0500

    Configure footer

---
+++ wp-content/themes/buitron/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage Buitron
+ * @since Buitron 1.0
+ */
+?>  
+
+    <?php wp_footer(); ?>
+</body>
+</html>
